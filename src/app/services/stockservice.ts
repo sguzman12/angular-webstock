62f9686b7f09c1ea8d8f324b7223b22fd0560600
Sigfredo Guzman <sigfredo.guzman@gmail.com>
2021-03-30 14:47:01 -0400

    Formatted stockservice.ts file

--- src/app/services/stockservice.ts
+++ src/app/services/stockservice.ts
@@ -1,21 +1,23 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpClientModule } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Companystock } from '../model/companystock';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StockService {
-  private apiServerUrl = environment.apiBaseUrl;  //'http://localhost:8080'
+  private apiServerUrl = environment.apiBaseUrl; //'http://localhost:8080'
 
-  constructor(private http: HttpClient) {  }
+  constructor(private http: HttpClient) {}
 
   /**
-  *  Returns observable of http request to api for company stocks
-  **/
-  public getStocks(): Observable<Companystock[]>{
-    return this.http.get<Companystock[]>(`${this.apiServerUrl}/company/companystock`);
+   *  Returns observable of http request to api for company stocks
+   **/
+  public getStocks(): Observable<Companystock[]> {
+    return this.http.get<Companystock[]>(
+      `${this.apiServerUrl}/company/companystock`
+    );
   }
 }
